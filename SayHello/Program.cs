bb5150723e6ad7ccf066b53f8fe1ef437bdbdf09
MarikaRomeo <marika.romeo.syne25lin@edu.tucsweden.se>
2025-09-15 10:40:28 +0200

    Add project files.

---
+++ SayHello/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt per l'utente 
+            Console.Write("Ange ditt namn: ");
+            string name = Console.ReadLine() ?? "";
+
+            // Chiamata al metodo
+            SayHello(name);
+        }
+
+        // Metodo che stampa  saluto
+        static void SayHello(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                Console.WriteLine("Hej! Trevligt att träffas."); // saluto generico se niente inserito
+            }
+            else
+            {
+                Console.WriteLine($"Hej, {name}!");
+            }
+        }
+    }
+}
